075af6b7f4b3e899f0f0e656a34a1797610868be
roman61185 <117517302+roman61185@users.noreply.github.com>
2023-03-31 16:38:39 +0300

    Update task-09.js
    изменил

--- js/task-09.js
+++ js/task-09.js
@@ -11,14 +11,33 @@ console.log(switchButton);
 
 // клик
 switchButton.addEventListener('click', () => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  spanColor.textContent = getRandomHexColor();
-})
+  const funGetColor = getRandomHexColor();
+  document.body.style.backgroundColor = funGetColor;
+  spanColor.textContent = funGetColor;
 
-
+});
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-
 }
+// const funGetColor = getRandomHexColor();
+
+
+
+
+// const multiBtn = document.querySelector("#multiple");
+
+// const firstCallback = () => {
+//   console.log("First callback!");
+// };
+// const secondCallback = () => {
+//   console.log("Second callback!");
+// };
+// const thirdCallback = () => {
+//   console.log("Third callback!");
+// };
+
+// multiBtn.addEventListener("click", firstCallback);
+// multiBtn.addEventListener("click", secondCallback);
+// multiBtn.addEventListener("click", thirdCallback);
